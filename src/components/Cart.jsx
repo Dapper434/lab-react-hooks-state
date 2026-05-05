ca76e6fa4bd886fa466e121fc46ae8815245de05
Pipsy <gpflaslik7112@gmail.com>
2026-05-06 00:04:24 +0300

    feat: add props to the Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Cart = () => {
+// receives the cart array from App and lists out what's been added
+const Cart = ({ cart }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
       <ul>
-        {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+        {cart.map(item => (
+          <li key={item.id}>{item.name} is in your cart.</li>
+        ))}
       </ul>
     </div>
   )
